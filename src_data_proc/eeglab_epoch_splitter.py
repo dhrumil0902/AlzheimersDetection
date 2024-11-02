ffe6caa8e855fbc058c593af341f4609f3ab70c7
Xiangyu <chenxy327@gmail.com>
2024-11-02 02:04:37 -0400

    add Python/MNE starter code (this concludes section 6.1.4 of the thesis)

--- src_data_proc/eeglab_epoch_splitter.py
+++ src_data_proc/eeglab_epoch_splitter.py
@@ -1,21 +1,33 @@
+import os
 from mne import Epochs, find_events
-from mne.datasets import sample
-from mne.io import read_raw_fif
+from mne.io import read_epochs_eeglab
 
 
-directory = sample.data_path() / "MEG" / "sample"
-raw = read_raw_fif(directory / "sample_audvis_raw.fif", preload=False)
-raw.pick_types(eeg=True, stim=True)
-raw.load_data()
+DATA_PATH = "../eeg_files_notch60_50_split5_12"
+SAMPLING_FREQUENCY_HZ = 500
+EPOCH_LENGTH_SECONDS = 12
+EPOCH_LENGTH_SAMPLES = EPOCH_LENGTH_SECONDS * SAMPLING_FREQUENCY_HZ
+N_CHANNELS = 19
+N_SUBJECTS = 88
+N_EPOCHS = 5
+N_SEGMENTS = 6
+SEGMENT_LENGTH_SAMPLES = int(EPOCH_LENGTH_SAMPLES / N_SEGMENTS)
 
-event_id = dict(left=1, right=2)
-events = find_events(raw, stim_channel="STI 014")
-epochs = Epochs(
-    raw,
-    events,
-    event_id=event_id,
-    tmin=-0.2,
-    tmax=0.5,
-    reject=None,
-    preload=True,
-)
+
+for subject_id in range(1, N_SUBJECTS + 1):
+    subject_str = f"{subject_id:03}"
+    dataset_path = os.path.join(DATA_PATH, f"split-{subject_str}.set")
+
+    epochs = read_epochs_eeglab(dataset_path)
+
+    for epoch_id in range(N_EPOCHS):
+        epoch = epochs[epoch_id].get_data()[0] #a numpy array
+        assert(epoch.shape == (N_CHANNELS, EPOCH_LENGTH_SAMPLES))
+
+        for segment_id in range(N_SEGMENTS):
+            start_sample = segment_id * SEGMENT_LENGTH_SAMPLES
+            end_sample = start_sample + SEGMENT_LENGTH_SAMPLES #exclusive
+            segment = epoch[:, start_sample:end_sample]
+
+            #TODO: do something with the segment
+            print(segment.shape)
